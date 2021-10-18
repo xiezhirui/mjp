48dffe2f859b9084b9311dd75076ccbe31fe52d3
xiezhirui <xiezr@cdhuari.com>
2021-10-19 03:46:42 +0800

    20211019-1

---
+++ src/main/java/com/cdhrtx/mjp/oop/interfaces/service/impl/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.cdhrtx.mjp.oop.interfaces.service.impl;
+
+import com.cdhrtx.mjp.oop.interfaces.service.UserService;
+
+/**
+ * 用户服务接口实现
+ * 
+ * @author xzr
+ *
+ */
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public void login() {
+		// TODO Auto-generated method stub
+		System.out.println("PC用户登录成功");
+	}
+
+}
---
+++ src/test/java/com/cdhrtx/mjp/oop/interfaces/service/UserServiceTest.java
@@ -0,0 +1,17 @@
+package com.cdhrtx.mjp.oop.interfaces.service;
+
+/**
+ * 用户服务接口测试用例
+ * 
+ * @author xzr
+ *
+ */
+public class UserServiceTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("当前的网站名称是：" + UserService.WEB_SITE_NAME);
+
+	}
+
+}
